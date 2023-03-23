7b363d81c56eb2eea5fbf6476defea63197a0bc0
rafapecino <rpecbon@myuax.com>
2023-03-23 19:09:48 +0100

    Ejercicio1

--- src/main/java/Practica1/Main.java
+++ src/main/java/Practica1/Main.java
@@ -1,2 +1,26 @@
-package Practica1;public class Main {
+package Practica1;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int puntosTotales = 1000000;
+        int aciertos = 0;
+        double radio = 1.0;
+        Random rnd = new Random();
+
+
+        for (int i = 0; i < puntosTotales; i++) {
+            double x = rnd.nextDouble() * 2 - 1; // número aleatorio entre -1 y 1
+            double y = rnd.nextDouble() * 2 - 1; // número aleatorio entre -1 y 1
+
+            if (Math.sqrt(x * x + y * y) <= radio) {
+                aciertos++;
+            }
+        }
+
+        double areaCuadrado = 4.0;
+        double aproximacionPi = areaCuadrado * aciertos / puntosTotales;
+        System.out.println("Aproximación al número pi: " + aproximacionPi);
+    }
 }
